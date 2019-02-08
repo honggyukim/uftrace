037a30aa3db6aff6b8d79c42a9d1750d4599b919
Honggyu Kim <honggyu.kp@gmail.com>
2019-02-09 06:12:07 +0900

    python: Fix import error by passing globs
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- misc/uftrace.py
+++ misc/uftrace.py
@@ -1,6 +1,16 @@
 import sys
 import trace_python
+import os
+
+progname = sys.argv[1]
+
+globs = {
+    '__file__': progname,
+    '__name__': '__main__',
+    '__package__': None,
+    '__cached__': None,
+}
 
 sys.settrace(trace_python.trace)
 
-execfile(sys.argv[1])
+exec(open(progname).read(), globs, globs)
