3e6fb47d140904cdd9b7d0f4551930a816e010c3
Honggyu Kim <honggyu.kp@gmail.com>
2019-03-31 10:32:28 +0900

    field: Combine address print functions
    This combines address print functions of replay, graph and tui commands
    into a single print_addr() in utils/field.c.
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- cmds/tui.c
+++ cmds/tui.c
@@ -220,14 +220,6 @@ static void print_graph_self(struct field_data *fd)
 	print_time(d);
 }
 
-static void print_graph_addr(struct field_data *fd)
-{
-	/* uftrace records (truncated) 48-bit addresses */
-	int width = sizeof(long) == 4 ? 8 : 12;
-
-	fd->print("%*lx", width, fd->addr);
-}
-
 static struct display_field field_total_time= {
 	.id      = GRAPH_F_TOTAL_TIME,
 	.name    = "total-time",
@@ -259,7 +251,7 @@ static struct display_field field_addr = {
 	.header  = "   ADDRESS  ",
 	.length  = 12,
 #endif
-	.print   = print_graph_addr,
+	.print   = print_addr,
 	.list    = LIST_HEAD_INIT(field_addr.list),
 };
 
