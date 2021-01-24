596911c6cae4cb24c2a9ac0c8b4df1f03004ea04
Honggyu Kim <honggyu.kp@gmail.com>
2021-01-24 23:50:29 +0000

    script: Make native script disable uftrace record
    This patch makes the given script to disable the actual uftrace record
    to shared memory.  It allows the script control the behavior of uftrace.
    
    Signed-off-by: Honggyu Kim <honggyu.kp@gmail.com>

--- libmcount/mcount.c
+++ libmcount/mcount.c
@@ -1096,7 +1096,7 @@ void mcount_exit_filter_record(struct mcount_thread_data *mtdp, struct mcount_re
 			mtdp->record_idx--;
 
 		if (!mcount_enabled)
-			return;
+			goto out;
 
 		if (!(rstack->flags & MCOUNT_FL_RETVAL))
 			retval = NULL;
@@ -1140,6 +1140,7 @@ void mcount_exit_filter_record(struct mcount_thread_data *mtdp, struct mcount_re
 				mtdp->nr_events = k; /* invalidate sync events */
 		}
 
+out:
 		/* script hooking for function exit */
 		if (script_str)
 			script_hook_exit(mtdp, rstack);
@@ -1713,6 +1714,8 @@ static void mcount_script_init(enum uftrace_pattern_type patt_type)
 
 	if (script_init(&info, patt_type) < 0)
 		script_str = NULL;
+	else if (!info.record)
+		mcount_enabled = false;
 }
 
 static __used void mcount_startup(void)
