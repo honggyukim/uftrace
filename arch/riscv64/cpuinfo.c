1b135020c242802f7c7facf5cac0a29eb984569d
Gichoel Choi <gichoel3101@gmail.com>
2023-08-27 17:46:34 +0900

    arch/riscv64: Update 'cpuinfo.c' for get architecture-dependent cpu info
    This is a commit to get CPU information using the '/proc/cpuinfo'
    file in a RISC-V 64bit environment.
    
    Signed-off-by: Gichoel Choi <gichoel3101@gmail.com>

--- arch/riscv64/cpuinfo.c
+++ arch/riscv64/cpuinfo.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-/* TODO: not implemented yet (Start) */
 int arch_fill_cpuinfo_model(int fd)
 {
-	return 0;
+	char buf[1024];
+	FILE *fp;
+	int ret = -1;
+
+	fp = fopen("/proc/cpuinfo", "r");
+	if (fp == NULL)
+		return -1;
+
+	while (fgets(buf, sizeof(buf), fp) != NULL) {
+		if (!strncmp(buf, "isa\t\t:", 6)) {
+			dprintf(fd, "cpuinfo:desc=%s", &buf[7]);
+			ret = 0;
+			break;
+		}
+	}
+
+	fclose(fp);
+	return ret;
 }
-/* TODO: not implemented yet (End) */
